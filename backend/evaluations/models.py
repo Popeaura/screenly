829c0f3b5c53fd1da9b536b1e388fb60d38aa352
Popeaura <popeahura@gmail.com>
2026-01-15 19:39:27 +0300

    I modied my projects folder

--- backend/evaluations/models.py
+++ backend/evaluations/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Candidate(models.model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    applied_for = models.CharField(max_length=200, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Evaluator(models.model):
+    first_name = models.CharField(max_length=100)     
