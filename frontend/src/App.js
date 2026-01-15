1b333c1dd794fb7f7e38bdefb5d10a621a1e70fe
Popeaura <popeahura@gmail.com>
2026-01-15 21:02:26 +0300

    I modied my projects folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Features from "./components/Features";
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-950 text-slate-50">
       <Navbar />
       <Hero />
+      <Features />
     </div>
   );
 }
