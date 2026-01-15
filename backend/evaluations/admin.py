87598ee0ca9ad3f96aef50f68e159132ff7c57c7
Popeaura <popeahura@gmail.com>
2026-01-15 20:25:50 +0300

    I modied my projects folder

--- backend/evaluations/admin.py
+++ backend/evaluations/admin.py
@@ -2,11 +2,20 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Candidate, Evaluator, Question, Evaluation, Result
+from .models import Candidate, Evaluator, Question
+from .models import Evaluation, Result
+
+class ResultInline(admin.TabularInline):
+    model = Result
+    extra = 1 
+
+class EvaluationAdmin(admin.ModelAdmin):
+    inlines = [ResultInline]
+    filter_horizontal = ('questions',)
 
 # Registering my  models here
 admin.site.register(Candidate)
 admin.site.register(Evaluator)
 admin.site.register(Question)
-admin.site.register(Evaluation)
+admin.site.register(Evaluation, EvaluationAdmin)
 admin.site.register(Result)
