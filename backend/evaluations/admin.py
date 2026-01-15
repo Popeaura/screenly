963285e1ea02585680c962f2c36454a7d43c1a47
Popeaura <popeahura@gmail.com>
2026-01-15 20:01:32 +0300

    I modied my projects folder

--- backend/evaluations/admin.py
+++ backend/evaluations/admin.py
@@ -3,3 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from .models import Candidate, Evaluator, Question, Evaluation, Result
+
+# Registering my  models here
+admin.site.register(Candidate)
+admin.site.register(Evaluator)
+admin.site.register(Question)
+admin.site.register(Evaluation)
+admin.site.register(Result)
