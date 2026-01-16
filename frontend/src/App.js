c9e6079de3d1ccbd63c8c9bc7a14f848bb0a7ba7
Popeaura <popeahura@gmail.com>
2026-01-16 09:49:45 +0300

    I modied my projects folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./index.css";
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import SignupPage from "./pages/SignupPage";
 
 const App = () => (
   <div className="app-root">
-    <LandingPage />
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+    </Routes>
   </div>
 );
 
