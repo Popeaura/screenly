05dd14ca21b714b3ca3d9368c5a2d69614c0b86b
Popeaura <popeahura@gmail.com>
2026-01-20 17:00:36 +0300

    I modied my projects folder

--- backend/screenly_api/urls.py
+++ backend/screenly_api/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # JWT auth
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # App APIs
     path('api/', include('evaluations.urls')),
 ]
