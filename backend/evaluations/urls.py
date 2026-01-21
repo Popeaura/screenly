a4a099bec97b28919efebd9274be97e9035f05d3
Popeaura <popeahura@gmail.com>
2026-01-21 17:14:56 +0300

    I modied my projects folder

--- backend/evaluations/urls.py
+++ backend/evaluations/urls.py
@@ -1,17 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    # JWT auth
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    # App APIs
-    path('api/', include('evaluations.urls')),
+    path('test/', views.test_api, name='test_api'),
 ]
