7a16abb0ee5f1e75b2b84494823835dc567023e6
Popeaura <popeahura@gmail.com>
2026-01-25 15:34:59 +0300

    I modied my projects folder

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -5,15 +5,12 @@ const api = axios.create({
 });
 
 // Attach token automatically
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("access_token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
