c39900c822efc1d17fa9fc116a6d95ee14ba47be
Popeaura <popeahura@gmail.com>
2026-01-25 15:38:56 +0300

    I modied my projects folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,28 @@
-import React from "react";
-import "./index.css";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 import LandingPage from "./pages/LandingPage";
-import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/DashboardPage";
-import PlaceholderPage from "./pages/PlaceholderPage";
-import CreateEvaluationPage from "./pages/CreateEvaluationPage";
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
 
-const App = () => (
-  <div className="app-root">
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/about" element={<PlaceholderPage title="About Us" />} />
-      <Route path="/courses" element={<PlaceholderPage title="Courses" />} />
-      <Route path="/contact" element={<PlaceholderPage title="Contact" />} />
-      <Route path="/evaluations/new" element={<CreateEvaluationPage />} />
-    </Routes>
-  </div>
-);
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
